a6f77b6c664d35672c2c5a59d3ad41aada4defd2
ahkubek <120653318+ahkubek@users.noreply.github.com>
2023-01-09 15:11:01 +0300

    цикл2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
         for(int i=0;i<10;i=i+1){
             System.out.println("итерация цикла"+i);
         }
+        System.out.println("Далее");
+
+        for (int i=2020; i<2070; i=i+4){
+            System.out.println("год высокостный"+i);
+        }
     }
 }
